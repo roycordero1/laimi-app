0dabd351730aa947eb46b68909aad6328f84cf34
humbertou <humbertou@users.noreply.github.com>
2017-09-23 22:32:36 -0600

    Cambio del controlador de cuenta para cambio de contraseña, falta terminarlo

--- LAIMIStock/Controllers/AccountController.cs
+++ LAIMIStock/Controllers/AccountController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LAIMIStock.Models;
 
 namespace LAIMIStock.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult ChangePassword()
+        public ActionResult ChangePassword(Usuarios userModel)
         {
-            return View();
+            using (laimistockappEntities db = new laimistockappEntities())
+            {
+                var userDetails = db.Usuarios.Where(x => x.nombre == userModel.nombre).FirstOrDefault();
+
+                return View();
+            }
         }
     }
 }
