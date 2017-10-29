d6ab9238253c312afd7e0817fd06f8904db02ed1
humbertou <humbertou@users.noreply.github.com>
2017-10-29 16:51:12 -0600

    se agrega la parte de los reportes aunque falta terminarlos

---
+++ LAIMIStock/Controllers/ReportsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LAIMIStock.Models;
+using System.Diagnostics;
+
+namespace LAIMIStock.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult ReportTotalValue()
+        {
+            laimistockappEntities db = new laimistockappEntities();
+            var reportDetails = db.reportTotalSupply().FirstOrDefault();
+            //Debug.WriteLine("Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            Debug.WriteLine(reportDetails);
+            ViewBag.TotalValue = reportDetails;
+            return View();
+        }
+
+        public ActionResult ReportAmountInventory()
+        {
+            laimistockappEntities db = new laimistockappEntities();
+            var reportDetails = db.reportTotalSupply().ToList();
+            ViewBag.AmountInventory = reportDetails;
+            return View();
+
+        }
+
+        public ActionResult ReportAssetsState()
+        {
+            return View();
+        }
+
+        public ActionResult ReportBlog()
+        {
+            return View();
+        }
+
+        public ActionResult ReportSuppliesPerLab()
+        {
+            return View();
+        }
+
+        public ActionResult ReportSupplyConsuption()
+        {
+            return View();
+        }
+    }
+}
