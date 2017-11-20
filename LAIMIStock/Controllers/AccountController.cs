7b0d9085bdf3595ab81fcc12f1977dfd044a818a
humbertou <humbertou@users.noreply.github.com>
2017-11-19 19:50:40 -0600

    se agrega la parte para encriptar contraseñas

--- LAIMIStock/Controllers/AccountController.cs
+++ LAIMIStock/Controllers/AccountController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using LAIMIStock.Models;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace LAIMIStock.Controllers
 {
@@ -19,9 +19,22 @@ namespace LAIMIStock.Controllers
         {
             using (laimistockappEntities db = new laimistockappEntities())
             {
-                var userDetails = db.Usuarios.Where(x => x.nombre == userModel.nombre && x.password == userModel.password).FirstOrDefault();
-                
-                
+                var user = db.Usuarios.Where(x => x.nombre == userModel.nombre).FirstOrDefault();
+                string salt = user.salt;
+                string passSh;
+                if (salt != null)
+                {
+                    passSh = sha256encrypt(userModel.password, salt, userModel.nombre)[0];
+                }
+
+                else
+                {
+                    passSh = sha256encrypt(userModel.password, "", userModel.nombre)[0];
+                }
+
+                var userDetails = db.Usuarios.Where(x => x.nombre == userModel.nombre && x.password == passSh).FirstOrDefault();
+
+
                 if (userDetails == null)
                 {
                     userModel.UpdatePasswordErrorMessage = "La contraseña no corresponde al usuario";
@@ -29,7 +42,9 @@ namespace LAIMIStock.Controllers
                 }
                 else
                 {
-                    userDetails.password = userModel.newPassword;
+                    string[] pass = sha256encrypt(userModel.newPassword, "", userModel.nombre);
+                    userDetails.password = pass[0];
+                    userDetails.salt = pass[1];
                     userDetails.nombre = userModel.nombre;
                     try
                     {
@@ -60,6 +75,49 @@ namespace LAIMIStock.Controllers
                 
             }
             
+        }
+        private static string[] sha256encrypt(string password, string pSalt, string userName)
+        {
+            string salt = "";
+            if (pSalt == "")
+            {
+                salt = CreateSalt(userName);
+            }
+            else
+            {
+                salt = pSalt;
+            }
+            string saltAndPwd = String.Concat(password, salt);
+            UTF8Encoding encoder = new UTF8Encoding();
+            SHA256Managed sha256hasher = new SHA256Managed();
+            byte[] hashedDataBytes = sha256hasher.ComputeHash(encoder.GetBytes(saltAndPwd));
+            string hashedPwd = String.Concat(byteArrayToString(hashedDataBytes), salt);
+            string[] pass = { hashedPwd, salt};
+            return pass;
+        }
+        public static string byteArrayToString(byte[] inputArray)
+        {
+            StringBuilder output = new StringBuilder("");
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                output.Append(inputArray[i].ToString("X2"));
+            }
+            return output.ToString();
+        }
+
+        private static string CreateSalt(string UserName)
+        {
+            string username = UserName;
+            byte[] userBytes; string salt;
+            userBytes = ASCIIEncoding.ASCII.GetBytes(username);
+            long XORED = 0x00; foreach (int x in userBytes)
+                XORED = XORED ^ x;
+            Random rand = new Random(Convert.ToInt32(XORED));
+            salt = rand.Next().ToString();
+            salt += rand.Next().ToString();
+            salt += rand.Next().ToString();
+            salt += rand.Next().ToString();
+            return salt;
         }
     }
 }
--- LAIMIStock/Controllers/LoginController.cs
+++ LAIMIStock/Controllers/LoginController.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using LAIMIStock.Models;
-using LAIMIStock.ViewModels;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace LAIMIStock.Controllers
 {
@@ -21,7 +20,20 @@ namespace LAIMIStock.Controllers
         {
             using (laimistockappEntities db = new laimistockappEntities())
             {
-                var userDetails = db.Usuarios.Where(x => x.nombre == userModel.nombre && x.password == userModel.password).FirstOrDefault();
+                var user = db.Usuarios.Where(x => x.nombre == userModel.nombre).FirstOrDefault();
+                string salt = user.salt;
+                string passSh;
+                if (salt != null)
+                {
+                    passSh = sha256encrypt(userModel.password, salt, userModel.nombre);
+                }
+
+                else
+                {
+                    passSh = sha256encrypt(userModel.password, "", userModel.nombre);
+                }
+                
+                var userDetails = db.Usuarios.Where(x => x.nombre == userModel.nombre && x.password == passSh).FirstOrDefault();
 
                 if (userDetails == null)
                 {
@@ -30,6 +42,7 @@ namespace LAIMIStock.Controllers
                 }
                 else
                 {
+                    
                     Session["usuarioID"] = userDetails.idUsuario;
                     Session["nombre"] = userDetails.nombre;
                     if (Convert.ToInt32(Session["usuarioID"]) == 2)
@@ -51,6 +64,49 @@ namespace LAIMIStock.Controllers
         {
             Session.Abandon();
             return RedirectToAction("Index", "Login");
+        }
+
+        private static string sha256encrypt(string password, string pSalt, string userName)
+        {
+            string salt = "";
+            if (pSalt == "")
+            {
+                salt = CreateSalt(userName);
+            }
+            else
+            {
+                salt = pSalt;
+            }
+            string saltAndPwd = String.Concat(password, salt);
+            UTF8Encoding encoder = new UTF8Encoding();
+            SHA256Managed sha256hasher = new SHA256Managed();
+            byte[] hashedDataBytes = sha256hasher.ComputeHash(encoder.GetBytes(saltAndPwd));
+            string hashedPwd = String.Concat(byteArrayToString(hashedDataBytes), salt);
+            return hashedPwd;
+        }
+        public static string byteArrayToString(byte[] inputArray)
+        {
+            StringBuilder output = new StringBuilder("");
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                output.Append(inputArray[i].ToString("X2"));
+            }
+            return output.ToString();
+        }
+
+        private static string CreateSalt(string UserName)
+        {
+            string username = UserName;
+            byte[] userBytes; string salt;
+            userBytes = ASCIIEncoding.ASCII.GetBytes(username);
+            long XORED = 0x00; foreach (int x in userBytes)
+                XORED = XORED ^ x;
+            Random rand = new Random(Convert.ToInt32(XORED));
+            salt = rand.Next().ToString();
+            salt += rand.Next().ToString();
+            salt += rand.Next().ToString();
+            salt += rand.Next().ToString();
+            return salt;
         }
     }
 }
